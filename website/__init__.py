734a471eb4b90a039a55542b70e3d884a064048c
danaqqq <ttt9@o2.pl>
2021-05-23 19:25:33 +0200

    cd

--- website/__init__.py
+++ website/__init__.py
@@ -30,7 +30,6 @@ def create_app():
     def load_user(id):
         return User.query.get(int(id))
 
-
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
     login_manager.init_app(app)
@@ -38,7 +37,6 @@ def create_app():
     @login_manager.user_loader
     def load_user(id):
         return User.query.get(int(id))
-
 
     return app
 
--- website/auth.py
+++ website/auth.py
@@ -44,8 +44,8 @@ def sign_up():
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
-        if len(email) < 4:
-            flash('Email must be greater than 3 characters.', category='error')
+        if len(email) < 5:
+            flash('Email must be greater than 4 characters.', category='error')
         elif len(first_name) < 2:
             flash('First name must be greater than 1 character.', category='error')
         elif password1 != password2:
@@ -55,7 +55,6 @@ def sign_up():
         else:
             new_user = User(email=email, first_name=first_name,
                             password=generate_password_hash(password1, method='sha256'))
-            #print(new_user)
             db.session.add(new_user)
             db.session.commit()
             login_user(new_user, remember=True)
